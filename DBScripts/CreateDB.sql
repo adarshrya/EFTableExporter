116418180bce32902ee407115485bff71f8a304a
Adarsha Acharya <adarshrya@gmail.com>
2022-02-12 01:22:56 +0530

    DB Scripts

---
+++ DBScripts/CreateDB.sql
@@ -0,0 +1,70 @@
+ 
+CREATE DATABASE [MasterDb]
+ 
+
+
+USE [MasterDb]
+GO
+
+/****** Object:  Table [dbo].[DataTable1]    Script Date: 12-02-2022 01:15:43 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[DataTable1](
+	[DataCol1] [nvarchar](100) NULL,
+	[DataCol2] [nvarchar](100) NULL,
+	[DataCol3] [nvarchar](100) NULL,
+	[DataCol4] [nvarchar](100) NULL,
+	[DataCol5] [nvarchar](100) NULL,
+	[DataCol6] [nvarchar](100) NULL,
+	[DataCol7] [nvarchar](100) NULL,
+	[DataCol8] [nvarchar](100) NULL,
+	[DataCol9] [nvarchar](100) NULL,
+	[DataCol10] [nvarchar](100) NULL,
+	[DataCol11] [nvarchar](100) NULL,
+	[DataCol12] [nvarchar](100) NULL,
+	[DataCol13] [nvarchar](100) NULL,
+	[DataCol14] [nvarchar](100) NULL,
+	[DataCol15] [nvarchar](100) NULL,
+	[DataCol16] [nvarchar](100) NULL,
+	[DataCol17] [nvarchar](100) NULL,
+	[DataCol18] [nvarchar](100) NULL,
+	[DataCol19] [nvarchar](100) NULL,
+	[DataCol20] [nvarchar](100) NULL,
+	[DataCol21] [nvarchar](100) NULL,
+	[DataCol22] [nvarchar](100) NULL,
+	[DataCol23] [nvarchar](100) NULL,
+	[DataCol24] [nvarchar](100) NULL,
+	[DataCol25] [nvarchar](100) NULL,
+	[DataCol26] [nvarchar](100) NULL,
+	[DataCol27] [nvarchar](100) NULL,
+	[DataCol28] [nvarchar](100) NULL,
+	[DataCol29] [nvarchar](100) NULL,
+	[DataCol30] [nvarchar](100) NULL,
+	[DataCol31] [nvarchar](100) NULL,
+	[DataCol32] [nvarchar](100) NULL,
+	[DataCol33] [nvarchar](100) NULL,
+	[DataCol34] [nvarchar](100) NULL,
+	[DataCol35] [nvarchar](100) NULL,
+	[DataCol36] [nvarchar](100) NULL,
+	[DataCol37] [nvarchar](100) NULL,
+	[DataCol38] [nvarchar](100) NULL,
+	[DataCol39] [nvarchar](100) NULL,
+	[DataCol40] [nvarchar](100) NULL,
+	[DataCol41] [nvarchar](100) NULL,
+	[DataCol42] [nvarchar](100) NULL,
+	[DataCol43] [nvarchar](100) NULL,
+	[DataCol44] [nvarchar](100) NULL,
+	[DataCol45] [nvarchar](100) NULL,
+	[DataCol46] [nvarchar](100) NULL,
+	[DataCol47] [nvarchar](100) NULL,
+	[DataCol48] [nvarchar](100) NULL,
+	[DataCol49] [nvarchar](100) NULL,
+	[DataCol50] [nvarchar](100) NULL
+) ON [PRIMARY]
+GO
+
+
